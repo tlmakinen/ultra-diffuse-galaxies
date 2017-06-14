c888bc2134cdc6d73e621f1d3496954ec0460451
tmakinen <tmakinen@princeton.edu>
2017-06-14 15:49:31 -0400

    Created the file FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+#FizzBuzz
+import numpy as mp
+
+mylist = np.arange(1, 100, 1)
+
+for n,i in enumerate(mylist):
+	if (i%3 == 0):
+		mylist[n]=Fizz
+for m,j in enumerate(mylist):
+	if (j%3 == 0 and j%5 == 0):
+		mylist[m] = FizzBuzz
+
+
