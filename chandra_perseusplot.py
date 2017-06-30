7bf7ad4f1292c52b5d79a9d3ae6349eae77a59aa
tlmakinen <tmakinen@princeton.edu>
2017-06-30 14:46:47 -0400

    colorbar added

--- chandra_perseusplot.py
+++ chandra_perseusplot.py
@@ -15,12 +15,13 @@ from astropy.io import fits
 import matplotlib.cm as cmx
 import matplotlib.colors as colors
 
-def image_file(imagenum):  # takes each file's number and returns a string filename
+# takes each file's number and returns a string filename #
+def image_file(imagenum):                                  
     fname = 'regfiles/%s.reg' %imagenum
 
     return fname
 
-
+# plot all the Wittmann and Song Perseus Cluster objects #
 def perseusplot(ax1):
 
     songhigh = Table.read('song-high-sb.csv')              # Takes in Whittmann's and Song's data
@@ -39,79 +40,53 @@ def perseusplot(ax1):
     plt.xlabel('$ra [deg]$')
     plt.ylabel('$dec [deg]$')
     plt.tight_layout()
-
-    #plt.show()
+    x1275 = [49.95041666]
+    y1275 = [41.51138889]
+    ax1.plot(x1275,y1275, '+r')                             # plot NGC1275 (center of Perseus Cluster)
 
     return ax1
 
-def panelplot(axis, ds9number, panelarray, colornum):
-
+# Creates a list of patches for each Chandra observation #
+def panelplot(axis, ds9number, panelarray, colornum):       
     ax1 = axis
-    imagenum = ds9number                                   # which file are we looking at?
-    
-    paneltags = []                                         # list of panel tags
+    imagenum = ds9number                                    # which file are we looking at? 
+    paneltags = []                                          # list of panel tags
     for j in range(len(panelarray)):
-        paneltags.append("tag={%s}" %panelarray[j])        # creates a reference tag for each panel number in the input
-
-        
-    r = pyregion.open(image_file(imagenum))                # Use pyregion.open to get the information from the region file
-
-    xyarrays = []                                          # stores the desireable panels' xy-coordinates from r[index]
+        paneltags.append("tag={%s}" %panelarray[j])         # creates a reference tag for each panel number in the input    
+    
+    r = pyregion.open(image_file(imagenum))                 # Use pyregion.open to get the information from the region file
+    xyarrays = []                                           # stores the desireable panels' xy-coordinates from r[index]
     commentarray = []
     
-
-    for i in range(len(r)):                                # choose the panels we want to plot
+    for i in range(len(r)):                                 # choose the panels we want to plot
         for j in range(len(paneltags)):
             if r[i].comment == paneltags[j]:
                 xyarrays.append(r[i].coord_list)
-                commentarray.append(r[i].comment)          #  Save each comment from selected panel (saves panel name)
+                commentarray.append(r[i].comment)           #  Save each comment from selected panel (saves panel name)
 
-
-    # upperlimit = 100
-    # values = range(upperlimit)
-    # jet = cm = plt.get_cmap('jet')
-    # cNorm = colors.Normalize(vmin=0, vmax=values[-1])
-    # scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
-    # print(scalarMap.get_clim())
-
-    # colorVal = scalarMap.to_rgba(colornum)
-
-
-
-    colorarray = np.zeros(len(commentarray))               #  Associate a color with each panel name
-    for c in range(len(commentarray)):
-        if "I3" in commentarray[c]:
-            colorarray[c] = colornum
-        elif "I2" in commentarray[c]:
-            colorarray[c] = colornum
-        elif "I1" in commentarray[c]:
-            colorarray[c] = colornum
-        elif "I0" in commentarray[c]:
-            colorarray[c] = colornum
-        elif "S3" in commentarray[c]:
-            colorarray[c] = colornum
-
-       
+    # colorarray = np.zeros(len(commentarray))               #  Associate a color with each panel name
+    # for c in range(len(commentarray)):
+    #     if "I3" in commentarray[c]:
+    #         colorarray[c] = colornum
+    #     elif "I2" in commentarray[c]:
+    #         colorarray[c] = colornum
+    #     elif "I1" in commentarray[c]:
+    #         colorarray[c] = colornum
+    #     elif "I0" in commentarray[c]:
+    #         colorarray[c] = colornum
+    #     elif "S3" in commentarray[c]:
+    #         colorarray[c] = colornum                                                               
     patches = []                                            # initialize list of patches
-    #edgecolors = [matplotlib.cm.jet(x) for x in colorarray]
     for i in range(len(xyarrays)):                          # hard-code the polynomials using four calculated panel points
         xy = xyarrays[i]
         panel = Polygon([[xy[0],xy[1]], [xy[2],xy[3]], [xy[4],xy[5]], [xy[6],xy[7]]], fill=False)  
         patches.append(panel)
-
-
-    #p = PatchCollection(patches, cmap=matplotlib.cm.jet, alpha=0.2, facecolor=edgecolors, edgecolors=edgecolors) # collect all the patches together
-    #p = PatchCollection(patches)
-    #colors = colorarray                                     
-    #p.set_array(np.array(colors))                           # set all the colors
-
-    
-    #ax1.add_collection(p)                                   # add all our patches to the axes
-    #plt.title(str(imagenum))                                # add what number we're looking at
-    #plt.show()
+    #if len(xyarrays) > 0:
+    #    print(imagenum)
     return patches
 
-def fits_file(imagenum):  # takes each file's number and returns a string fits filename
+ # takes each file's number and returns a string FITS filename #
+def fits_file(imagenum):                                    
     if int(imagenum) < 1000:
         return 'FITSfiles/acisf00%s_repro_evt2.fits' %imagenum
     if int(imagenum) < 10000:
@@ -119,45 +94,51 @@ def fits_file(imagenum):  # takes each file's number and returns a string fits f
     else:
         return 'FITSfiles/acisf%s_repro_evt2.fits' %imagenum
 
-chandrapanels = ['I3', 'S3', 'I2', 'I1', 'I0']
-nums = [502, 4949, 17274]
 
-nums = [int(n.split('.')[0]) for n in os.listdir('regfiles/') if n[-1]=='g']  # Plot all .reg fules in regfiles/
-#print(nums)
+# Create plot of all panels by exposure time #
+
+chandrapanels = ['I1', 'I0', 'I2', 'I3', 'S3']
+nums = [11713, 11715, 11714]
+#nums = [int(n.split('.')[0]) for n in os.listdir('regfiles/') if n[-1]=='g']  # Plot all .reg fules in regfiles/
+print(nums)
 
 
 fig, ax1 = plt.subplots()
 perseusplot(ax1)
-timearray = []
 
-for i in range(len(nums)):                               # plot all panel files specified
-    fname = fits_file(nums[i])
-    hdulist = fits.open(fname)
-    hduheader = hdulist[1].header
+figtitle = 'Chandra panels over Perseus data by exposure time [ksecs]'
+t = fig.text(0.45, 0.97, figtitle, horizontalalignment='center')
+                                                            
+timearray = []                                              # list to store exposure times from each fits file
+newnums = []
+for i in range(len(nums)):                                
+    fname = fits_file(nums[i])                              # open the designated fits file header
+    hdulist = fits.open(fname) 
+    hduheader = hdulist[1].header                           # get the second header for the data required
 
-    exposuretime = hduheader['EXPOSURE']                  # get the exposure time from the FITS header
-    colornum = exposuretime                        # adjust exposure time to ksecs
-                                                          # also adjusts for a [0,100] color map
-    timearray.append(colornum)
+    exposuretime = hduheader['EXPOSURE']                    # get the exposure time from the FITS header
+    colornum = exposuretime/1000                            # adjust exposure time to ksecs
+    upperlimit = 140
+    lowerlimit = 50                                          # set desired upper limit to exposure time to plot only panels in threshold
+    if colornum > lowerlimit and colornum < upperlimit:                               
+        newnums.append(nums[i])                             # use only obs IDs that fall under upper limit                            
+        timearray.append(colornum)                          # append the color index array
 
 colorindex = np.array(timearray)
-index_norm = colorindex / colorindex.max()
+index_norm = colorindex / colorindex.max()                  # Normalize color range to work with the colormap
 
-for i in range(len(nums)):
-    #print("exposure time of " + str(nums[i]) + " = " + str(exposuretime))
-    rightcolor = index_norm[i]
-    patches = panelplot(ax1, nums[i], chandrapanels, rightcolor)
-    edgecolors = [matplotlib.cm.jet(rightcolor)]
-
+for i in range(len(index_norm)):                            # plot all panel files specified
+    print("exposure time of " + str(newnums[i]) + " = " + str(timearray[i]))
+    rightcolor = index_norm[i]                                      # use normalized color index for our plots
+    patches = panelplot(ax1, newnums[i], chandrapanels, rightcolor)    # call panelplot for each observation number
+    edgecolors = [matplotlib.cm.jet(rightcolor)]                       
     p = PatchCollection(patches, cmap=matplotlib.cm.jet, alpha=0.2, facecolor=edgecolors, edgecolors=edgecolors) # collect all the patches together
-    #p.set_array()
-    ax1.add_collection(p) 
+    ax1.add_collection(p)                                    # add collection of panels to the plot
 
-
-    #plt.title(str(nums[loop]))
-
-
-
+p.set_array(np.array(colorindex))                            # Set the colorbar, using the last "p" patch collection limits for the scale (same for all collections)
+#p.set_clim([np.ma.min(colorindex), np.ma.max(colorindex)])   # Set the colorbar's min and max values (in ksecs)
+p.set_clim([lowerlimit, upperlimit])
+plt.colorbar(p, shrink=0.5)
 
 plt.show()
 
